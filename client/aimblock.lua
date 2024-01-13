e2e05fcf86617afbcd8e292dec1d0cb7e180ca95
Griefa <harleygcameron996@gmail.com>
2024-01-14 00:33:41 +1000

    feat(aimblock): disable killing players with huntingrifle

---
+++ client/aimblock.lua
@@ -0,0 +1,38 @@
+local hasHuntingRifle = false
+local isFreeAiming = false
+local blockShotActive = false
+
+local function aimBlock()
+    if blockShotActive then return end
+    blockShotActive = true
+    CreateThread(function()
+        while hasHuntingRifle do
+            Wait(0)
+            local player = PlayerId()
+            local entity = nil
+            local aiming, entity = GetEntityPlayerIsFreeAimingAt(player)
+            local freeAiming = IsPlayerFreeAiming(player)
+            local type = GetEntityType(entity)
+            if not freeAiming or IsPedAPlayer(entity) or type == 2 or (type == 1 and IsPedInAnyVehicle(entity, false)) then
+                DisableControlAction(0, 24, true)
+                DisableControlAction(0, 47, true)
+                DisableControlAction(0, 58, true)
+                DisablePlayerFiring(PlayerPedId(), true)
+            end
+        end
+        blockShotActive = false
+    end)
+end
+
+CreateThread(function()
+    local huntingrifle = `weapon_huntingrifle`
+    while true do
+        Wait(0)
+        if GetSelectedPedWeapon(PlayerPedId()) == huntingrifle then
+            hasHuntingRifle = true
+            aimBlock()
+        else
+            hasHuntingRifle = false
+        end
+    end
+end)
